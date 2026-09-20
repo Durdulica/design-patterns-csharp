c6df8dceb3388a16c682332742cd048b85b2248a
Stefan Scarlat <stefanel.scarlat@gmail.com>
2026-09-20 20:45:35 +0300

    testare

--- Program.cs
+++ Program.cs
@@ -14,16 +14,16 @@ internal class Program
 {
     private static void Main()
     {
-        //Testare1 testare1 = new();
-        //Testare2 testare2 = new();
-        //Testare3 testare3 = new();
-        //Testare4 testare4 = new();
-        //Testare5 testare5 = new();
-        //Testare6 testare6 = new();
-        //Testare7 testare7 = new();
-        //Testare8 testare8 = new();
-        //Testare9 testare9 = new();
+        Testare1 testare1 = new();
+        Testare2 testare2 = new();
+        Testare3 testare3 = new();
+        Testare4 testare4 = new();
+        Testare5 testare5 = new();
+        Testare6 testare6 = new();
+        Testare7 testare7 = new();
+        Testare8 testare8 = new();
+        Testare9 testare9 = new();
         Testare10 testare10 = new();
-        //Testare11 testare11 = new();
+        Testare11 testare11 = new();
     }
 }
